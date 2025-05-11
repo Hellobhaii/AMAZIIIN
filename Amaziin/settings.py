a34f79906f9f99e7d778e42d63636ebe669d0a5c
Hellobhaii <145827615+Hellobhaii@users.noreply.github.com>
2025-05-11 20:56:41 +0530

    Update settings.py

--- Amaziin/settings.py
+++ Amaziin/settings.py
@@ -9,7 +9,11 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+"""
+Amaziin - E-commerce Website
+Developed by Chirag Agarwal, JECRC University, 2024.
+Encoded ID: Q2hpcmFnIEFnYXJ3YWwgMjAyNCBKRUNTUiBVbml2ZXJzaXR5
+"""
 import os
 from pathlib import Path
 
